a248b4e29a3e2fa5dd3684c704e16f0040c32dec
Grimnir7 <luca.querciagrossa@hftm.ch>
2024-06-09 18:36:19 +0200

    add more plants to db

--- db/add_plants.py
+++ db/add_plants.py
@@ -1,27 +1,46 @@
 import sqlite3
 
 # Verbindung zur SQLite-Datenbank herstellen
-conn = sqlite3.connect('plants.db')
+conn = sqlite3.connect('db/plants.db')
 
 # Cursor-Objekt erstellen, um SQL-Befehle auszuführen
 cursor = conn.cursor()
 
 # Neue Pflanzen hinzufügen (Beispielwerte)
 pflanzen = [
-    ("Kopfsalat", 30.0, 30.0, 0.1),
-    ("Randen", 10.0, 30.0, 2.0),
-    ("Ruebli", 1.0, 30.0, 1.0)
+    ("Buschbohnen", 5.0, 40.0, 1.0),
+    ("Peperoni lang", 40.0, 40.0, 0.5),
+    ("Glockenpeperoni", 60.0, 60.0, 0.5),
+    ("Chili", 40.0, 40.0, 0.5),
+    ("Kohlrabi", 25.0, 25.0, 0.5),
+    ("Broccoli", 40.0, 40.0, 0.5),
+    ("Rosenkohl", 50.0, 50.0, 0.5),
+    ("Tomaten", 80.0, 80.0, 0.5),
+    ("Cherry-Tomaten", 80.0, 80.0, 0.5),
+    ("Basilikum", 30.0, 30.0, 0.5),
+    ("Petersilie", 2.0, 20.0, 0.5),
+    ("Dill", 2.0, 15.0, 0.5)
+
+
 ]
 
 # SQL-Befehl zum Einfügen von Pflanzen
 insert_query = "INSERT INTO Pflanzen (name, pflanzabstand, reihenabstand, saattiefe) VALUES (?, ?, ?, ?)"
 
 # Pflanzen in die Tabelle einfügen
-cursor.executemany(insert_query, pflanzen)
+for pflanze in pflanzen:
+    # Überprüfen, ob die Pflanze bereits vorhanden ist
+    cursor.execute("SELECT COUNT(*) FROM Pflanzen WHERE name = ?", (pflanze[0],))
+    result = cursor.fetchone()
+    
+    if result[0] > 0:
+        print(f"Pflanze '{pflanze[0]}' ist bereits in der Datenbank vorhanden.")
+    else:
+        cursor.execute(insert_query, pflanze)
+        print(f"Pflanze '{pflanze[0]}' erfolgreich zur Datenbank hinzugefügt.")
 
-# Aenderungen speichern und Verbindung schliessen
+# Änderungen speichern und Verbindung schließen
 conn.commit()
 conn.close()
 
-print("Neue Pflanzen erfolgreich zur Datenbank hinzugefuegt.")
-
+print("Überprüfung und Hinzufügen von Pflanzen abgeschlossen.")
